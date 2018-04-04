c36c883ffe75955121e6d853afa6d3b5805272e7
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2018-04-04 15:56:15 -0500

    Se termino la parte del login , aun falta implementar el registrar pero eso en teoria no debe estar.Aun asi se va a implementar para que el aplicativo se vea mejor

---
+++ app/src/main/java/pe/anthony/sidp/app/MyApplication.java
@@ -0,0 +1,50 @@
+package pe.anthony.sidp.app;
+
+import android.app.Application;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+import pe.anthony.sidp.models.User;
+
+/**
+ * Created by ANTHONY on 4/04/2018.
+ */
+public class MyApplication extends Application{
+
+    public  static AtomicInteger UserId = new AtomicInteger();
+
+    //Voy a configurar cosas global de la app y esto se va configurar cuando se inice la app
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setUpRealmConfig();
+        Realm realm = Realm.getDefaultInstance();
+        UserId = getIdByTable(realm, User.class);
+        realm.close();
+    }
+
+    private void setUpRealmConfig(){
+        // The default Realm file is "default.realm" in Context.getFilesDir();
+        Realm.init(getApplicationContext());
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name(Realm.DEFAULT_REALM_NAME)
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(config);
+    }
+
+    private <T extends RealmObject> AtomicInteger getIdByTable(Realm realm, Class<T> anyClass){
+        RealmResults<T> result = realm.where(anyClass).findAll();
+        //si hay mas de un registro lo que signnifica que hay un valor de ID asignado
+        if(result.size()> 0) {
+            return new AtomicInteger(result.max("id").intValue());
+        }else {
+            return new AtomicInteger();
+        }
+    }
+}
---
+++ app/src/main/java/pe/anthony/sidp/models/User.java
@@ -0,0 +1,63 @@
+package pe.anthony.sidp.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+import pe.anthony.sidp.app.MyApplication;
+
+/**
+ * Created by ANTHONY on 4/04/2018.
+ */
+
+public class User extends RealmObject{
+
+    @PrimaryKey
+    private int id;
+
+    @Required
+    private String username;
+
+    @Required
+    private String password;
+
+    @Required
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email){
+        this.id= MyApplication.UserId.incrementAndGet();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
