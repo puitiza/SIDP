216705ae8458909e027022d072b657881f08f8d6
apuitiza <anthony.puitiza.02@gmail.com>
2018-06-14 16:21:14 -0500

    guargando ProductEntity

--- app/src/main/java/pe/anthony/sidp/data/entities/ProductEntity.java
+++ app/src/main/java/pe/anthony/sidp/data/entities/ProductEntity.java
@@ -2,7 +2,6 @@ package pe.anthony.sidp.data.entities;
 
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
-import io.realm.annotations.Required;
 import pe.anthony.sidp.app.MyApplication;
 
 /**
